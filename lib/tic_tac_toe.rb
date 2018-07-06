3c994bb43ca0cca1b2378a445f56e3eb14bed9e0
Chris Franklin <cf73021@gmail.com>
2018-07-06 16:55:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,11 +17,11 @@ WIN_COMBINATIONS = [
 
 def display_board(board)
   board = @board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n"
   puts "-----------\n"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} \n"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} \n"
   puts "-----------\n"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
 
 
