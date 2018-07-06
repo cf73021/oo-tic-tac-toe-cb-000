e8e85a15eeb07ec603700896be1b7be8661449bf
Chris Franklin <cf73021@gmail.com>
2018-07-06 17:08:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,4 +36,8 @@ def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
 
+def valid_move?(board, index)
+ index.between?(0,8) && !position_taken?(board, index)
+end
+
 end
